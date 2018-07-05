e90d6d7b585fd043f34a05296106196154772532
degski <degski@gmail.com>
2018-07-05 09:24:46 +0300

    license notifications

--- practrand/main.cpp
+++ practrand/main.cpp
@@ -1,3 +1,11 @@
+/**
+* Copyright degski 2018
+* Distributed under the Boost Software License, Version 1.0. (See
+* accompanying file LICENSE_1_0.txt or copy at
+* http://www.boost.org/LICENSE_1_0.txt)
+*
+* See http://www.boost.org for most recent version including documentation.
+*/
 
 #ifdef _WIN32 // needed to allow binary stdout on windows
 #include <fcntl.h>
